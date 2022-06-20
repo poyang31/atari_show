1dbf577c64812357ac8f1577955db1bf34a54f22
poyang31 <poyang31@yahoo.com.tw>
2022-06-20 13:41:42 +0800

    fix http.js

--- src/client/http.js
+++ src/client/http.js
@@ -1,4 +1,4 @@
-const apiServer = process.env.REACT_APP_ATARI_API_HOST || "http://localhost:3000";
+const apiServer = process.env.REACT_APP_ATARI_API_HOST || "https://atari.fuseita.com";
 
 const generalConfig = {
     method: "GET",
